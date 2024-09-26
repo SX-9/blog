c2b18b79ac5ebb3d4c3fe98ecf5f167ad6315295
root <root@dev.local>
2024-09-26 18:52:01 +0700

    finnished

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,17 @@
-// place files you want to import through the `$lib` alias in this folder.
+import PocketBase from 'pocketbase';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+
+const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+export default pb;
+
+export function formatTime(strdate: string): string {
+    const date = new Date(strdate);
+    const timeMs = typeof date === "number" ? date : date.getTime();
+    const deltaSeconds = Math.round((timeMs - Date.now()) / 1000);
+    const cutoffs = [60, 3600, 86400, 86400 * 7, 86400 * 30, 86400 * 365, Infinity];
+    const units: Intl.RelativeTimeFormatUnit[] = ["second", "minute", "hour", "day", "week", "month", "year"];
+    const unitIndex = cutoffs.findIndex(cutoff => cutoff > Math.abs(deltaSeconds));
+    const divisor = unitIndex ? cutoffs[unitIndex - 1] : 1;
+    const rtf = new Intl.RelativeTimeFormat('en-US', { numeric: "auto" });
+    return rtf.format(Math.floor(deltaSeconds / divisor), units[unitIndex]);
+}
